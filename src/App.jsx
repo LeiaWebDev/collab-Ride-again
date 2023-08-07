43c1abe168c8859a27d1d3f534af171102f1b10f
LeiaWebDev <leia@cowmeuh.net>
2023-08-07 14:10:33 +0200

    react pages added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+///////  not yet done any changes, this is the default original file when created /////////
+
 function App() {
   const [count, setCount] = useState(0)
 
---
+++ src/pages/CreateAd.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CreateAd() {
+  return (
+    <div>CreateAd</div>
+  )
+}
+
+export default CreateAd
