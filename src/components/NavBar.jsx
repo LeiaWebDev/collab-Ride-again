9f40d3c24250c34f0babbea09ebb6ff6d5b3ee39
Conrad 宮文軒 <mackayleblanc.conrad@gmail.com>
2023-08-08 08:02:07 +0200

    90% of the HomePAge is done. We can display 3 photos in each of the three categories. Need to be styled now. Navbar almost done. Just need to create a component to not repeat the code of bikes information

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,32 @@
-import React from "react"
-import { NavLink } from "react-router-dom"
+import React from "react";
+import { NavLink } from "react-router-dom";
+// import "./navbar.css";
 function NavBar({ removeUser }) {
-	const user = JSON.parse(localStorage.getItem("user"))
+    // const user = JSON.parse(localStorage.getItem("user"));
 
-	return (
-		<nav>
-			<NavLink to={"/"}>Home</NavLink>
-			{/* <NavLink to={"/stuff"}>Things with arrays and fav's</NavLink> */}
-			{!user && (
-				<>
-					<NavLink to={"/signup"}>Signup</NavLink>
-					<NavLink to={"/login"}>Login</NavLink>
-				</>
-			)}
-			{user && <button onClick={removeUser}>Logout</button>}
-		</nav>
-	)
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <img src="/src/assets/minions-sleepy-help-me-kg8b2gdcdn4pmlpd.gif" alt="Logo" />
+                <button>Add an ad</button>
+            </div>
+            <div className="navbar-links">
+                {/* <NavLink to={"/"}>Home</NavLink> */}
+                {/* <NavLink to={"/stuff"}>Things with arrays and fav's</NavLink> */}
+                {/* {!user && (
+                    <>
+                        <NavLink to={"/signup"}>Signup</NavLink>
+                        <NavLink to={"/login"}>Login</NavLink>
+                    </>
+                )}
+                {user && (
+                    <>
+                        <button onClick={removeUser}>Logout</button>
+                    </>
+                )} */}
+            </div>
+        </nav>
+    );
 }
 
-export default NavBar
+export default NavBar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,174 @@
-import React from "react"
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+
+// function HomePage() {
+//     // on line 7, we are using the useState hook to create a state variable called randomCategories
+//     const [randomCategories, setRandomCategories] = useState({
+//         mechanical: [],
+//         electric: [],
+//         electricAssist: [],
+//     });
+
+//     let responseCopy = [...response.data];
+
+//     for (let i = 0; i < 3; i++) {
+//         const randomId = Math.random() * responseCopy.length;
+//         const chosenItem = responseCopy.splice(randomId, 1);
+//         randomCategories.push(chosenItem);
+//     }
+
+//     useEffect(() => {
+//         axios
+
+//             .get("https://ride-again.adaptable.app/ads?type=Electric Assist")
+//             .then((response) => {
+//                 const shuffled = response.data.sort(() => 0.5 - Math.random());
+//                 console.log(response.data);
+
+//                 const selected = shuffled.slice(0, 3);
+//                 setRandomCategories({ ...randomCategories, electricAssist: selected });
+//             })
+//             .catch((error) => {
+//                 console.log("Error fetching data:", error);
+//             });
+//     }, []);
+
+//     useEffect(() => {
+//         axios
+
+//             .get("https://ride-again.adaptable.app/ads?type=Mechanical")
+//             .then((response) => {
+//                 const shuffled = response.data.sort(() => 0.5 - Math.random());
+//                 console.log(response.data);
+//                 const selected = shuffled.slice(0, 3);
+//                 setRandomCategories({ ...randomCategories, electricAssist: selected });
+//             })
+//             .catch((error) => {
+//                 console.log("Error fetching data:", error);
+//             });
+//     }, []);
+
+//     useEffect(() => {
+//         console.log("Updated randomCategories:", randomCategories);
+//     }, [randomCategories]);
+
+//     if (!randomCategories) {
+//         return <div className="loading">flute flute flute</div>;
+//     }
+
+//     return (
+//         <div className="homePage">
+//             <h1>Hundreds of classified ads ELECTRICS</h1>
+
+//             <div className="random-categories">
+//                 {randomCategories.electricAssist.map((category) => (
+//                     <section key={category.id}>
+//                         <h2>{category.id}</h2>
+
+//                         <img src={category.image} alt={category.type} />
+//                     </section>
+//                 ))}
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default HomePage;
+
+// 1 we import moduls
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function HomePage() {
-	return <div>HomePage</div>
+    // 2 on line 86, we are using useState hook to create a state variable called randomCategories
+    const [randomCategories, setRandomCategories] = useState({
+        mechanical: [],
+        electric: [],
+        electricAssist: [],
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const mechanicalResponse = await axios.get("https://ride-again.adaptable.app/ads?type=Mechanical");
+                const electricResponse = await axios.get("https://ride-again.adaptable.app/ads?type=Electric");
+                const electricAssistResponse = await axios.get(
+                    "https://ride-again.adaptable.app/ads?type=Electric%20Assist"
+                );
+
+                const mechanicalAds = shuffleArray(mechanicalResponse.data).slice(0, 3);
+                const electricAds = shuffleArray(electricResponse.data).slice(0, 3);
+                const electricAssistAds = shuffleArray(electricAssistResponse.data).slice(0, 3);
+
+                setRandomCategories({
+                    mechanical: mechanicalAds,
+                    electric: electricAds,
+                    electricAssist: electricAssistAds,
+                });
+            } catch (error) {
+                console.log("Error fetching data:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+
+    if (
+        !randomCategories.mechanical.length ||
+        !randomCategories.electric.length ||
+        !randomCategories.electricAssist.length
+    ) {
+        return <div className="loading">Loading...</div>;
+    }
+
+    return (
+        <div className="homePage">
+            <div className="random-categories">
+                <h1>Hundreds of classified ads MECHANICAL</h1>
+                {randomCategories.mechanical.map((category) => (
+                    <section key={category.id}>
+                        <h2>{category.model}</h2>
+                        <p>Type: {category.type}</p>
+                        <p>Price: {category.price}</p>
+                        <img src={category.image} alt={category.type} />
+                    </section>
+                ))}
+            </div>
+
+            <div className="random-categories">
+                <h1>Hundreds of classified ads ELECTRIC</h1>
+                {randomCategories.electric.map((category) => (
+                    <section key={category.id}>
+                        <h2>{category.model}</h2>
+                        <p>Type: {category.type}</p>
+                        <p>Price: {category.price}</p>
+                        <img src={category.image} alt={category.type} />
+                    </section>
+                ))}
+            </div>
+
+            <div className="random-categories">
+                <h1>Hundreds of classified ads ELECTRIC ASSIST</h1>
+                {randomCategories.electricAssist.map((category) => (
+                    <section key={category.id}>
+                        <h2>{category.model}</h2>
+                        <p>Type: {category.type}</p>
+                        <p>Price: {category.price}</p>
+                        <img src={category.image} alt={category.type} />
+                    </section>
+                ))}
+            </div>
+        </div>
+    );
 }
 
-export default HomePage
+export default HomePage;
